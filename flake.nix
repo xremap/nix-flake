2b66318fc23f01aeaeda5e6f4db1a16115588bca
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-20 19:35:59 -0800

    chore: bump to xremap 0.8.1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     naersk.url = "github:nmattia/naersk/master";
     # The Rust source for xremap
     xremap = {
-      url = "github:k0kubun/xremap?ref=v0.8.0";
+      url = "github:k0kubun/xremap?ref=v0.8.1";
       flake = false;
     };
     hyprland = {
