1bfd36324ab48427a563b2c60e209db09202e000
Vladimir Timofeenko <id@vtimofeenko.com>
2023-11-30 15:51:31 -0800

    chore: move to crane for building the package
    Crane makes it easier to reuse artifacts when building multiple features
    at the same time.
    
    (almost) scientific test showed reduction of build time from ~5 minutes
    (naersk) to ~2 (crane).
    
    This should not change user experience.

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,16 +1,30 @@
 # Overlay file that contains the definition of building a package
-{ xremap, naersk-lib, pkgs }:
+{ xremap, craneLib, pkgs }:
 let
-  packageWithFeature = feature: naersk-lib.buildPackage {
-    root = xremap;
-    cargoBuildOptions = (x: x ++ pkgs.lib.optional (feature != null) "--features ${feature}");
+  commonArgs = {
+    src = xremap;
+    strictDeps = true;
+
+    buildInputs = [
+      # Add additional build inputs here
+    ];
+
+    # Additional environment variables can be set directly
+    # MY_CUSTOM_VAR = "some value";
+  };
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+  packageWithFeature = feature: craneLib.buildPackage (commonArgs // {
+    inherit cargoArtifacts;
+    cargoExtraArgs = "--locked${if (feature != null) then " --features ${feature}"  else ""}";
+    # cargoBuildOptions = (x: x ++ pkgs.lib.optional (feature != null) "--features ${feature}");
     # The following two options are for introspection to be able to see if sway/gnome were actually pulled in
     # To see that - visually inspect the deps directory inside result/target/ and check for swayipc/zbus
     # See cargo.toml for feature-specific deps
-    copyTarget = true;
-    compressTarget = false;
+    # copyTarget = true;
+    # compressTarget = false;
     meta.mainProgram = "xremap";
-  };
+  });
 in
 rec {
   # No features
