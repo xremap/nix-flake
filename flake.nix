c6ccf60535ca3085529e7619e3054c107ecf7fba
Vladimir Timofeenko <id@vtimofeenko.com>
2024-07-20 09:27:16 -0700

    chore: bump xremap input to 0.10.0

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
 
     # The Rust source for xremap
     xremap = {
-      url = "github:k0kubun/xremap?ref=v0.9.0";
+      url = "github:k0kubun/xremap?ref=v0.10.0";
       flake = false;
     };
     hyprland = {
