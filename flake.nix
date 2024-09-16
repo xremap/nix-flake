35fb5e014f60a291bdfb93862f3eb90498accb6a
Vladimir Timofeenko <id@vtimofeenko.com>
2024-09-16 12:44:28 -0700

    chore: remove crane nixpkgs override -- no longer exists in upstream

--- flake.nix
+++ flake.nix
@@ -12,10 +12,8 @@
 
     # Utils for building Rust stuff
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
+
     # fenix = {
     #   url = "github:nix-community/fenix";
     #   inputs.nixpkgs.follows = "nixpkgs";
