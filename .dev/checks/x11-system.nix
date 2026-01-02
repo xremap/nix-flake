58ca3e824ec986b4a8fa4ae607625bf58b5b8f3c
Vladimir Timofeenko <id@vtimofeenko.com>
2026-01-01 16:13:58 -0800

    feat(dev): add x11-system check

---
+++ .dev/checks/x11-system.nix
@@ -0,0 +1,54 @@
+{ self, ... }:
+{ testers, procps }:
+testers.runNixOSTest {
+  name = "Test xremap running in X11 in system mode";
+
+  skipTypeCheck = true;
+
+  nodes.machine =
+    { ... }:
+    {
+      imports = [
+        (import ../demo-nixos-configurations/x11-system.nix { inherit self; })
+        { virtualisation.qemu.networkingOptions = [ ]; }
+        {
+          systemd.network.wait-online.enable = false;
+          networking.useDHCP = false;
+          networking.interfaces = { };
+          systemd.services.network-setup.enable = false;
+        }
+        {
+          services.xremap.config.keymap = [
+            {
+              application.only = "kitty";
+              remap."9" = "0";
+            }
+          ];
+        }
+      ];
+    };
+
+  testScript = /* python */ ''
+    machine.wait_for_x(timeout=10)
+    # machine.send_chars("Hyprland\n")
+    machine.wait_until_succeeds("${procps}/bin/pgrep xremap", timeout=10)
+    machine.wait_for_unit("xremap.service", timeout=10)
+    machine.wait_for_unit("graphical-session.target", "alice", timeout=10)
+
+    # Start kitty
+    machine.sleep(1)
+    machine.execute("kitty >&2 &")
+    machine.wait_for_window("root@machine")
+    # machine.wait_until_succeeds("${procps}/bin/pgrep kitty", timeout=10)
+
+    machine.sleep(2) # Wait for it to fully start
+    output_file = "/tmp/xremap-output"
+    machine.send_chars(f"echo -n '9' > {output_file}\n")
+    machine.sleep(2)
+    output = machine.execute(f"cat {output_file}")[1]
+    expected_output = "0"
+
+    assert output == expected_output, f"Not the expected symbol. Expected: '{expected_output}' found: '{output}'!"
+
+  '';
+}
