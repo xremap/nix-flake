abbd07e92837c797ae1b98a1ff2c95fc805fe7b1
Vladimir Timofeenko <id@vtimofeenko.com>
2026-07-20 12:44:19 -0700

    fix: order xremap user unit after graphical-session.target
    This should start xremap after a WM starts, initializing the socket
    address.
    
    In practice this fixes application-specific maps not working when xremap
    user service starts initially
    
    Fixes #97

--- modules/user-service.nix
+++ modules/user-service.nix
@@ -21,7 +21,7 @@ in
       path = [ cfg.package ];
       # NOTE: xremap needs DISPLAY:, WAYLAND_DISPLAY: and a bunch of other stuff in the environment to launch graphical applications (launch:)
       # On Gnome after gnome-session.target is up - those variables are populated
-      after = lib.mkIf cfg.withGnome [ "gnome-session.target" ];
+      after = [ "graphical-session.target" ] ++ (lib.optional cfg.withGnome "gnome-session.target");
       wantedBy = [ "graphical-session.target" ];
       serviceConfig = mkMerge [
         {
