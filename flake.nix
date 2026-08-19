e1b0c29cccb77724705e4b4b5dc8cb32ee765661
Vladimir Timofeenko <id@vtimofeenko.com>
2026-08-19 13:57:07 -0700

    chore: bump xremap input to 0.15.11

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
     # The Rust source for xremap
     xremap = {
-      url = "github:k0kubun/xremap?ref=v0.15.10";
+      url = "github:k0kubun/xremap?ref=v0.15.11";
       flake = false;
     };
   };
